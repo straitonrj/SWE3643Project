5c506c4ee18371b77d8ce6cf5081e43609322df4
RJ Straiton <94383945+straitonrj@users.noreply.github.com>
2024-03-19 18:54:14 -0400

    Add files via upload

---
+++ BlazorApp1/CalculatorEngine Unit Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CalculatorEngine_Unit_Tests;
+using CalculatorEngine;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ BlazorApp1/CalculatorEngine/Class1.cs
@@ -0,0 +1,94 @@
+﻿namespace CalculatorEngine;
+using System;
+
+public static class CalculatorEngine
+{
+    static float Add(float inputA, float inputB)
+    {
+        //preq-Engine-3
+        return inputA + inputB;
+    }
+
+    static float Subtract(float inputA, float inputB)
+    {
+        //preq-Engine-4
+        return inputA - inputB;
+    }
+
+    static float Multiply(float inputA, float inputB)
+    {
+        //preq-Engine-5
+        return inputA * inputA;
+    }
+    
+    static float Divide(float inputA, float inputB)
+    {
+        //preq-Engine-7
+        if (inputB == 0)
+        {
+            throw new Exception("Cannot Divide by zero");
+        }
+        else
+        {
+            return inputA / inputB;
+        }
+    }
+    
+    //preq-Engine-8 Equals must go here
+    
+    static float Exponent(float inputA, float inputB)
+    {
+        //preq-Engine-9
+        return (float)Math.Pow(inputA, inputB);
+    }
+    //Need to put root method here, should root use exponent? Add inputB by itself until it reaches one?
+
+    static float Log(float inputA, float inputB)
+    {
+        //preq-Engine-10
+        return (float)Math.Log(inputA, inputB);
+    }
+    //A only Methods are as follows:
+    
+    //Tested in separate application, works
+    static float Factorial(float inputA)
+    {
+        //preq-Engine-12
+        if (inputA == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            inputA = inputA * Factorial(inputA - 1);
+            return inputA;
+        }
+    }
+
+    static float Sin(float inputA)
+    {
+        //preq-Engine-13
+        return (float)Math.Sin(inputA);
+    }
+
+    static float Cos(float inputA)
+    {
+        //preq-Engine-14
+        return (float)Math.Cos(inputA);
+    }
+
+    static float Tan(float inputA)
+    {
+        //preq-Engine-15
+        return (float)Math.Tan(inputA);
+    }
+
+    static float Reciprocal(float inputA)
+    {
+        //preq-Engine-16
+        return 1 / inputA;
+    }
+    
+    
+    
+}
